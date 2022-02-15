dc083132d815baa9ec1ad7e6fcbc79a1cea47d97
PikaLorie <lorie.rousselle@gmail.com>
2022-02-15 16:23:37 +0100

    Scene2.0

--- Runner/Assets/Scripts/Objects/MagnetCoins.cs
+++ Runner/Assets/Scripts/Objects/MagnetCoins.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class MagnetCoins : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float MagnetDuration = 0f;
+    public Collider Collider;
+
+   /* private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.layer == 10)
+        {
+            Collider.enabled = false;
+            SuperJumpDuration = 15f;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }
+        if (SuperJumpDuration != 0)
+        {
+            SuperJumpDuration = SuperJumpDuration - 1f * Time.deltaTime;
+        }
+
+        if (SuperJumpDuration <= 0)
+        {
+            playermovements.jumpForce = 5f;
+        }
+
+
+    }*/
 }
